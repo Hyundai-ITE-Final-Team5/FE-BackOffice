9ba1067b309f1b6e1a7da0eac6d9c24f12351f48
hyk339 <hyk339@gmail.com>
2021-12-09 15:44:37 +0900

    feat(event): event 추가, 수정부분 추가

--- ite5-fe-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/service/PromotionService.java
+++ ite5-fe-backoffice/src/main/java/com/ite5pjtbackoffice/backoffice/service/PromotionService.java
@@ -1,11 +1,13 @@
 package com.ite5pjtbackoffice.backoffice.service;
 
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ite5pjtbackoffice.backoffice.dto.EventSearchOption;
 import com.ite5pjtbackoffice.backoffice.dto.PagerAndEvents;
+import com.ite5pjtbackoffice.backoffice.vo.Event;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,5 +26,51 @@ public class PromotionService {
 			.bodyToMono(PagerAndEvents.class)
 			.block();
 		return pagerAndEvents;
+	}
+	
+	public Event getEvent(String eno) {
+		WebClient webClient = WebClient.create();
+		Event event = webClient
+				.post()
+				.uri("http://kosa1.iptime.org:50515/admin/promotion/eventdetail")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.bodyValue(eno)
+				.retrieve()
+				.bodyToMono(Event.class)
+				.block();
+		return event;
+	}
+	
+	public String updateEvent(Event event) {
+		WebClient webClient = WebClient.create();
+		String json = webClient
+				.put()
+				.uri("http://kosa1.iptime.org:50515/admin/promotion/eventupdate")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.bodyValue(event)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+		JSONObject jsonObject = new JSONObject(json);
+		String result = jsonObject.getString("result");
+		return result;
+	}
+	
+	public String insertEvent(Event event) {
+		WebClient webClient = WebClient.create();
+		String json = webClient
+				.post()
+				.uri("http://kosa1.iptime.org:50515/admin/promotion/eventinsert")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.bodyValue(event)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+		JSONObject jsonObject = new JSONObject(json);
+		String result = jsonObject.getString("result");
+		return result;
 	}
 }
